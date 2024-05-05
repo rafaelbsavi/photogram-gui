b76343833e7b4a41964f2f6e2e0ddf6ff51a5fb9
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-05-05 23:31:07 +0000

    photos page

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,15 @@
+class PhotosController < ApplicationController
+
+  def index
+    @list_of_photos = Photo.all.order({ :created_at => :desc})
+
+    render({ :template => "photo_templates/index"})    
+  end
+
+  def show
+    render({:template => "photo_templates/show"})
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  # User
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
 
-
-
+  #Photos
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:path_id", {:controller => "photos", :action => "show"})
 
 
 end
