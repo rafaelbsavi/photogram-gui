9c76087e06c30baeeb0b175fac3de3110dcd33c9
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-05-06 01:00:00 +0000

    add photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,9 +15,37 @@ class PhotosController < ApplicationController
     else
       render({:template => "photo_templates/show"})
     end
-    
-
   end
 
+  def delete
+    photo_id = params.fetch("photo_id")
+    photo_delete = Photo.where({ :id => photo_id}).at(0)
+
+    photo_delete.destroy
+
+    redirect_to("/photos")
+  end
+
+  def update
+    # {"input_image"=>"A", "input_caption"=>"B", "update_id"=>"785"}
+    
+  end
+
+  def insert
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    input_owner_id = params.fetch("input_owner_id")
+
+    new_photo = Photo.new
+
+    new_photo.image = input_image
+    new_photo.caption = input_caption
+    new_photo.owner_id = input_owner_id
+
+    new_photo.save
+
+    redirect_to("/photos/" + new_photo.id.to_s)
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  #Homepage
+  get("/", {:controller => "users", :action => "index"})
+
   # User
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
@@ -7,6 +10,9 @@ Rails.application.routes.draw do
   #Photos
   get("/photos", {:controller => "photos", :action => "index"})
   get("/photos/:path_id", {:controller => "photos", :action => "show"})
+  get("delete_photo/:photo_id", {:controller => "photos", :action => "delete"})
+  post("update_photo/:update_id", {:controller => "photos", :action => "update"})
+  post("insert_photo", {:controller => "photos", :action => "insert"})
 
 
 end
